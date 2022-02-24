773f93e1d667c3c3ccff5c227e23ea32b844b2b4
Christian Wiese <christian.wiese@freiheit.com>
2022-02-24 20:56:08 +0100

    Further work

--- christian/main.go
+++ christian/main.go
@@ -4,9 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
+
+var days = []int64{0}
 
 func main() {
 	input := ParseInput(os.Args[1])
@@ -14,15 +17,91 @@ func main() {
 	fmt.Println("WORKING ON: ", os.Args[1])
 
 	fmt.Println(input.numProjects, input.numContributors)
-	for _, c := range input.contributors {
-		fmt.Println(c)
-	}
-	for _, p := range input.projects {
-		fmt.Println(p)
-	}
+	//for _, c := range input.contributors {
+	//	fmt.Println(c)
+	//}
+	//for _, p := range input.projects {
+	//	fmt.Println(p)
+	//}
+
+	projects := input.projects
+	contributors := input.contributors
+
+	//sort.SliceStable(projects, func(i, j int) bool {
+	//	return projects[i].score*projects[j].numDays*projects[j].numRoles > projects[j].score*projects[i].numDays*projects[i].numRoles
+	//})
+	sort.SliceStable(projects, func(i, j int) bool {
+		return projects[j].numRoles > projects[i].numRoles
+	})
+	//sort.SliceStable(projects, func(i, j int) bool {
+	//	return projects[i].numDays < projects[j].numDays
+	//})
+
 	res := Result{}
 
+	for len(days) > 0 {
+		fmt.Println(days[0])
+		sort.SliceStable(days, func(i, j int) bool {
+			return days[i] < days[j]
+		})
+		for _, p := range projects {
+			if p.done {
+				continue
+			}
+			names := FindContributors(p, contributors, days[0])
+			if len(names) < len(p.roles) {
+				continue
+			}
+			p.done = true
+			days = append(days, days[0]+p.numDays)
+			res = append(res, &Assignment{projectName: p.name, contributors: names})
+		}
+		days = days[1:]
+	}
+
+	for _, p := range res {
+		fmt.Println(p)
+	}
+
 	Dump(res, os.Args[1])
+}
+
+func FindContributors(p *Project, cc []*Contributor, day int64) []string {
+	names := []string{}
+	for _, role := range p.roles {
+		found := false
+		for _, c := range cc {
+			if c.occupiedUntil > day {
+				continue
+			}
+			if c.skills[role.name] == role.level {
+				names = append(names, c.name)
+				c.occupiedUntil = day + p.numDays
+				c.skills[role.name]++
+				found = true
+				break
+			}
+		}
+		if found {
+			continue
+		}
+		minLevel := int64(100000000)
+		minID := -1
+		for id, c := range cc {
+			if c.occupiedUntil > day {
+				continue
+			}
+			if c.skills[role.name] > role.level && c.skills[role.name] < minLevel {
+				minLevel = c.skills[role.name]
+				minID = id
+			}
+		}
+		if minID >= 0 {
+			cc[minID].occupiedUntil = day + p.numDays
+			names = append(names, cc[minID].name)
+		}
+	}
+	return names
 }
 
 func Dump(o Result, file string) {
@@ -42,39 +121,46 @@ func Dump(o Result, file string) {
 	w.Flush()
 }
 
-func ParseInt(v string) int {
+func ParseInt(v string) int64 {
 	x, err := strconv.Atoi(v)
 	if err != nil {
 		panic(err)
 	}
-	return x
+	return int64(x)
 }
 
 type Input struct {
-	numContributors int
-	numProjects     int
+	numContributors int64
+	numProjects     int64
 	contributors    []*Contributor
 	projects        []*Project
 }
 
 type Contributor struct {
-	name      string
-	numSkills int
-	skills    Skills
+	name          string
+	numSkills     int64
+	skills        Skills
+	occupiedUntil int64
 }
 
-type Skills map[string]int
+type Skills map[string]int64
 
 type Project struct {
 	name       string
-	numDays    int
-	score      int
-	bestBefore int
-	numRoles   int
-	roles      Skills
+	numDays    int64
+	score      int64
+	bestBefore int64
+	numRoles   int64
+	roles      []*ProjectRole
+	done       bool
 }
 
-type Result []Assignment
+type ProjectRole struct {
+	name  string
+	level int64
+}
+
+type Result []*Assignment
 
 type Assignment struct {
 	projectName  string
@@ -99,7 +185,7 @@ func ParseInput(path string) *Input {
 	scanner.Scan()
 	input.numProjects = ParseInt(scanner.Text())
 
-	for i := 0; i < input.numContributors; i++ {
+	for i := int64(0); i < input.numContributors; i++ {
 		c := &Contributor{
 			skills: Skills{},
 		}
@@ -109,7 +195,7 @@ func ParseInput(path string) *Input {
 		scanner.Scan()
 		c.numSkills = ParseInt(scanner.Text())
 
-		for j := 0; j < c.numSkills; j++ {
+		for j := int64(0); j < c.numSkills; j++ {
 			scanner.Scan()
 			skillName := scanner.Text()
 			scanner.Scan()
@@ -118,10 +204,8 @@ func ParseInput(path string) *Input {
 		input.contributors = append(input.contributors, c)
 	}
 
-	for i := 0; i < input.numProjects; i++ {
-		p := &Project{
-			roles: Skills{},
-		}
+	for i := int64(0); i < input.numProjects; i++ {
+		p := &Project{}
 
 		scanner.Scan()
 		p.name = scanner.Text()
@@ -134,11 +218,15 @@ func ParseInput(path string) *Input {
 		scanner.Scan()
 		p.numRoles = ParseInt(scanner.Text())
 
-		for j := 0; j < p.numRoles; j++ {
+		for j := int64(0); j < p.numRoles; j++ {
 			scanner.Scan()
 			skillName := scanner.Text()
 			scanner.Scan()
-			p.roles[skillName] = ParseInt(scanner.Text())
+			skillLevel := ParseInt(scanner.Text())
+			p.roles = append(p.roles, &ProjectRole{
+				name:  skillName,
+				level: skillLevel,
+			})
 		}
 		input.projects = append(input.projects, p)
 	}
