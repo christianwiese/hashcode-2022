e5afe31bdc6cbebda2ef2047d53f1b6215e68f5e
Christian Wiese <christian.wiese@freiheit.com>
2022-02-24 19:14:29 +0100

    Write input parser

--- christian/main.go
+++ christian/main.go
@@ -13,10 +13,16 @@ func main() {
 
 	fmt.Println("WORKING ON: ", os.Args[1])
 
-	fmt.Println(input)
-	res := Result{}
-
-	Dump(res, os.Args[1])
+	fmt.Println(input.numProjects, input.numContributors)
+	for _, c := range input.contributors {
+		fmt.Println(c)
+	}
+	for _, p := range input.projects {
+		fmt.Println(p)
+	}
+	//res := Result{}
+	//
+	//Dump(res, os.Args[1])
 }
 
 func Dump(o Result, file string) {
@@ -49,6 +55,27 @@ func ParseInt(v string) int {
 }
 
 type Input struct {
+	numContributors int
+	numProjects     int
+	contributors    []*Contributor
+	projects        []*Project
+}
+
+type Contributor struct {
+	name      string
+	numSkills int
+	skills    Skills
+}
+
+type Skills map[string]int
+
+type Project struct {
+	name       string
+	numDays    int
+	score      int
+	bestBefore int
+	numRoles   int
+	roles      Skills
 }
 
 type Result struct {
@@ -67,16 +94,54 @@ func ParseInput(path string) *Input {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanWords)
 
-	//scanner.Scan()
-	//input.duration = ParseInt(scanner.Text())
-	//scanner.Scan()
-	//input.intersections = ParseInt(scanner.Text())
-	//scanner.Scan()
-	//input.numOfStreets = ParseInt(scanner.Text())
-	//scanner.Scan()
-	//input.numOfCars = ParseInt(scanner.Text())
-	//scanner.Scan()
-	//input.bonusPoints = ParseInt(scanner.Text())
+	scanner.Scan()
+	input.numContributors = ParseInt(scanner.Text())
+	scanner.Scan()
+	input.numProjects = ParseInt(scanner.Text())
+
+	for i := 0; i < input.numContributors; i++ {
+		c := &Contributor{
+			skills: Skills{},
+		}
+
+		scanner.Scan()
+		c.name = scanner.Text()
+		scanner.Scan()
+		c.numSkills = ParseInt(scanner.Text())
+
+		for j := 0; j < c.numSkills; j++ {
+			scanner.Scan()
+			skillName := scanner.Text()
+			scanner.Scan()
+			c.skills[skillName] = ParseInt(scanner.Text())
+		}
+		input.contributors = append(input.contributors, c)
+	}
+
+	for i := 0; i < input.numProjects; i++ {
+		p := &Project{
+			roles: Skills{},
+		}
+
+		scanner.Scan()
+		p.name = scanner.Text()
+		scanner.Scan()
+		p.numDays = ParseInt(scanner.Text())
+		scanner.Scan()
+		p.score = ParseInt(scanner.Text())
+		scanner.Scan()
+		p.bestBefore = ParseInt(scanner.Text())
+		scanner.Scan()
+		p.numRoles = ParseInt(scanner.Text())
+
+		for j := 0; j < p.numRoles; j++ {
+			scanner.Scan()
+			skillName := scanner.Text()
+			scanner.Scan()
+			p.roles[skillName] = ParseInt(scanner.Text())
+		}
+		input.projects = append(input.projects, p)
+	}
 
 	return input
 }
